bcb79108c5a8197e24b080414a7487f7a9fa98b1
Danila Kondrashen <Dankondrashen@yandex.com>
2024-06-20 07:14:37 +0300

    serverflask

--- ServerDiplom/reqparsers/expToDoc_reqparse.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import reqparse
-
-parserlink = reqparse.RequestParser()
-parserlink.add_argument('docId')
-parserlink.add_argument('expId')
-parserlink.add_argument('documents_scan')
